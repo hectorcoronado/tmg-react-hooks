0790853eb34d43e451252261c0fc34f164599562
hectorcoronado <hectorcoronado.developer@gmail.com>
2020-02-22 17:14:12 -0800

    customHooks - solution for useWindowDimensions.js

---
+++ customHooks/useWindowDimensions.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react"
+import ReactDOM from "react-dom"
+
+import "./styles.css"
+
+/*
+  Instructions:
+    You're given a `useWindowDimensions` custom Hook. Your
+    job is to finish implementing it. It should return
+    an object with a `width` property that represents the current
+    width of the window and a `height` property which represents
+    the current height. 
+
+    To get those values, you can use teh `window.addEventListener`
+    API.https://developer.mozilla.org/en-US/docs/Web/API/Window/resize_event
+*/
+
+const useWindowDimensions = () => {
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+
+  const handleChange = () => {
+    return setDimensions({
+      height: window.innerHeight,
+      width: window.innerWidth
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleChange, true)
+
+    return () => window.removeEventListener('resize', handleChange, true)
+  })
+
+  return dimensions
+}
+
+
+const App = () => {
+  const { width, height } = useWindowDimensions()
+
+  return (
+    <div className="App">
+      <h2>width: {width}</h2>
+      <h2>height: {height}</h2>
+      <p>Resize the window.</p>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
