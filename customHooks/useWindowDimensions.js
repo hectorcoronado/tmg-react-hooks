6aef6317fa205070d10ff3ba2cc1ee8d545c5346
hectorcoronado <hectorcoronado.developer@gmail.com>
2020-02-22 17:20:09 -0800

    customHooks - tmg solution for useWindowDimensions.js

--- customHooks/useWindowDimensions.js
+++ customHooks/useWindowDimensions.js
@@ -33,7 +33,7 @@ const useWindowDimensions = () => {
 
     return () => window.removeEventListener('resize', handleChange, true)
   })
-
+ 
   return dimensions
 }
 
@@ -52,3 +52,38 @@ const App = () => {
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(<App />, rootElement)
+
+/**
+ * tmg solution
+ */
+
+function useWindowDimensions() {
+  const [width, setWidth] = useState(window.innerWidth)
+  const [height, setHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    const listener = () => {
+      setWidth(window.innerWidth)
+      setHeight(window.innerHeight)
+    }
+
+    window.addEventListener('resize', listener)
+
+    return () => window.removeEventListener('resize', listener)
+  })
+
+  return {width, height}
+}
+
+
+function App() {
+  const { width, height } = useWindowDimensions()
+
+  return (
+    <div className="App">
+      <h2>width: {width}</h2>
+      <h2>height: {height}</h2>
+      <p>Resize the window.</p>
+    </div>
+  )
+}
